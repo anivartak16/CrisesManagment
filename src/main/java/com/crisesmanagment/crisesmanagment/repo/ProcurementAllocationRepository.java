2696147a4d133c73948f755b028b18dbc8a603a2
Tamanna <tamannag030@gmail.com>
2026-08-21 14:43:12 +0530

    do it saved

---
+++ src/main/java/com/crisesmanagment/crisesmanagment/repo/ProcurementAllocationRepository.java
@@ -0,0 +1,7 @@
+package com.crisesmanagment.crisesmanagment.repo;
+
+import com.crisesmanagment.crisesmanagment.model.ProcurementAllocation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProcurementAllocationRepository extends JpaRepository<ProcurementAllocation, Long> {
+}
